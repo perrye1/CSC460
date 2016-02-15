4b05b902c7658ee8501126eeff91368b16acafb5
Ethan Perry <perrye1@nku.edu>
2016-02-14 23:58:41 -0500

    finished logic

--- csc460_prog1/src/client.java
+++ csc460_prog1/src/client.java
@@ -1,33 +1,135 @@
 import java.io.*;
 import java.net.*;
 
-public class client
-{
+public class client{
 		
-	public static void main( String[] args)
-	{		
+	public static void main( String[] args){		
 		
 	  Socket toserversocket;
 		
-	  System.out.println("CLIENT is attempting connection....");
+	  System.out.println("CLIENT is waiting to connect....");
 	  try {
 		toserversocket = new Socket("localhost", 4030);
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		DataOutputStream outclient = new DataOutputStream(toserversocket.getOutputStream( ));
+		DataOutputStream out = new DataOutputStream(toserversocket.getOutputStream( ));
+		DataInputStream in = new DataInputStream(toserversocket.getInputStream( ));
 		
-		System.out.println("CONNECTION HAS BEEN MADE");
-		
-		
-		while(true){
+		Boolean gameOver = false;
+		String board[][] = new String[][]{
+				{" "," "," "},
+				{" "," "," "},
+				{" "," "," "}
+				};
+				
+		while(gameOver != true){
 			
-			System.out.println("Enter String: ");
-			String s = br.readLine();
-			System.out.println(s);
-			outclient.writeChars(s);
+			String serverMove = in.readUTF();
+			String clientMove;
+			String parts[];
+			
+			if(serverMove.equals("NONE")){
+				
+				printBoard(board);
+				System.out.println("Enter move in the format row-column");
+				clientMove = br.readLine();
+				
+				while(!(board[Integer.parseInt(clientMove.split("-")[0])][Integer.parseInt(clientMove.split("-")[1])].equals(" "))){
+					
+					System.out.println("move invalid!");
+					System.out.println("Enter move in the format row-column");
+					clientMove = br.readLine();
+					
+				}
+				
+				board[Integer.parseInt(clientMove.split("-")[0])][Integer.parseInt(clientMove.split("-")[1])] = "O";
+				
+				out.writeUTF("MOVE "+Integer.parseInt(clientMove.split("-")[0])+" "+Integer.parseInt(clientMove.split("-")[1]));
+			}else{
+				
+				parts = serverMove.split(" ");//parse servers move
+				
+				if(parts.length == 4){
+					
+					if(parts[3].equals("WIN")){
+						
+						//the server is confirming that the clients last move resulted in a win
+						printBoard(board); //show the board
+						System.out.println("You Won");
+						gameOver = true; //end the game
+						
+					}else if(parts[3].equals("LOSS")){
+						
+						board[Integer.parseInt(parts[1])][Integer.parseInt(parts[2])] = "X"; //modify local copy of board
+						printBoard(board); //show the board
+						System.out.println("You Lost");
+						gameOver = true; //end the game
+						
+					}else if(parts[3].equals("TIE")){
+						
+						if(parts[1].equals("0") & parts[1].equals("0")){
+							
+							if(board[0][0].equals(" ")){
+								
+								//the servers last move is actually at 0,0, and it results in a tie
+								board[Integer.parseInt(parts[1])][Integer.parseInt(parts[2])] = "X"; //modify local copy of board
+								printBoard(board); //show the board
+								System.out.println("The game ended in a Tie");
+								gameOver = true; //end the game
+								
+							}else{
+								
+								//the server is confirming that the clients last move resulted in a tie
+								printBoard(board); //show the board
+								System.out.println("The game ended in a Tie");
+								gameOver = true; //end the game
+								
+							}
+						}else{
+							
+							board[Integer.parseInt(parts[1])][Integer.parseInt(parts[2])] = "X"; //modify local copy of board
+							printBoard(board); //show the board
+							System.out.println("The game ended in a Tie");
+							gameOver = true; //end the game
+						}
+					}
+				}else{
+					
+					parts = serverMove.split(" ");//parse servers move
+					board[Integer.parseInt(parts[1])][Integer.parseInt(parts[2])] = "X"; //modify local copy of board
+					printBoard(board); //show the board
+					
+					System.out.println("Enter move in the format row-column");
+					clientMove = br.readLine();
+					
+					while(!(board[Integer.parseInt(clientMove.split("-")[0])][Integer.parseInt(clientMove.split("-")[1])].equals(" "))){
+						
+						System.out.println("move invalid!");
+						System.out.println("Enter move in the format row-column");
+						clientMove = br.readLine();
+						
+					}
+					
+					board[Integer.parseInt(clientMove.split("-")[0])][Integer.parseInt(clientMove.split("-")[1])] = "O";
+					
+					out.writeUTF("MOVE "+Integer.parseInt(clientMove.split("-")[0])+" "+Integer.parseInt(clientMove.split("-")[1]));
+					
+				}
+			}
 			
 		}
 	  }
  	  catch (IOException  e) {   };
-         }
+   }
+	
+	public static void printBoard(String[][] board){
+		System.out.println(" ");
+		System.out.println(board[0][0] + " | " + board[0][1] + " | " + board[0][2]);
+		System.out.println("---------");
+		System.out.println(board[1][0] + " | " + board[1][1] + " | " + board[1][2]);
+		System.out.println("---------");
+		System.out.println(board[2][0] + " | " + board[2][1] + " | " + board[2][2]);
+		System.out.println(" ");
+	}
 }
+
--- csc460_prog1/src/server.java
+++ csc460_prog1/src/server.java
@@ -1,6 +1,6 @@
 import java.net.*;
-import java.io.*;   
-
+import java.io.*; 
+import java.util.Random;
 
 public class server
 {
@@ -20,27 +20,118 @@ public class server
 		while (true){
 			System.out.println("waiting for connection");
 			toclientsocket = SS.accept();   // block UNTIL request received
-			boolean gameOver = false;
-			DataInputStream in = new DataInputStream(toclientsocket.getInputStream( ));
-			String recieved = "";
-			
 			System.out.println("connection made");
 			
+			DataOutputStream out = new DataOutputStream(toclientsocket.getOutputStream( ));
+			DataInputStream in = new DataInputStream(toclientsocket.getInputStream());
+			
+			int moveCount = 0;
+			boolean gameOver = false;
+			boolean serverMoveFirst;
+			Random rand = new Random();
+			
+			String board[][] = new String[][]{
+					{" "," "," "},
+					{" "," "," "},
+					{" "," "," "}
+					};
+			
+			String clientMove;
+			String serverMove;
+			String parts[];
+			
+			if(rand.nextInt(2) == 1) {
+			    serverMoveFirst = true;
+			}else{
+				serverMoveFirst = false;
+			}
+			
+			if(serverMoveFirst){
+				//generate a move and send to client
+				serverMove = generateMove(board);
+				out.writeUTF(serverMove);
+				System.out.println("server sent: "+serverMove);
+				moveCount++;
+			}else{
+				//tell the client to move first
+				out.writeUTF("NONE");
+				System.out.println("server sent: NONE");
+			}
+			
 			while(!gameOver==true){
+			
+				clientMove = in.readUTF();
+				System.out.println("client sent: "+clientMove);
+				parts = clientMove.split(" ");//parse clients move
 				
-				recieved = in.readUTF();
-				if(recieved.equals("exit")){
-					gameOver=true;
+				board[Integer.parseInt(parts[1])][Integer.parseInt(parts[2])] = "O";
+				moveCount++;
+				if(checkWin(board).equals("O")){
+					out.writeUTF("MOVE 0 0 WIN");
+					System.out.println("server sent: MOVE 0 0 WIN");
+					gameOver = true;
+				}else if(moveCount==9){
+					out.writeUTF("MOVE 0 0 TIE");
+					System.out.println("server sent: MOVE 0 0 TIE");
+					gameOver = true;
 				}else{
-					System.out.println(recieved);
+					serverMove = generateMove(board);
+					moveCount++;
+					if(checkWin(board).equals("X")){
+						out.writeUTF(serverMove+" LOSS");
+						System.out.println("server sent: "+serverMove+" LOSS");
+						gameOver = true;
+					}else if(moveCount==9){
+						out.writeUTF(serverMove+" TIE");
+						System.out.println("server sent: "+serverMove+" TIE");
+						gameOver = true;
+					}else{
+						out.writeUTF(serverMove);
+						System.out.println("server sent: "+serverMove);
+					}
 				}
 				
-			}
-			 
-			
+			}			
 		}
-		
     		}   // end try
     		catch (IOException e) {}  
 	}  // end main
-}  // end myserver class
+	
+	public static String generateMove(String[][] board){
+		
+		Random rand = new Random();
+		int pos1 = rand.nextInt(3);
+		int pos2 = rand.nextInt(3);
+		while(!board[pos1][pos2].equals(" ")){
+			pos1 = rand.nextInt(3);
+			pos2 = rand.nextInt(3);		
+		}
+		
+		board[pos1][pos2] = "X";
+		
+		return "MOVE "+pos1+" "+pos2;
+		
+	}
+	
+	public static String checkWin(String[][] board){
+		for(int i = 0; i < 3; i++){
+			 boolean h = board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2]);
+			 boolean v = board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i]);
+			 if(h){
+				 return board[i][0];
+			 }
+			 if(v){
+				 return board[0][i];
+			 }
+		}
+		
+		boolean diag1 = board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2]);
+		boolean diag2 = board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0]);
+		if(diag1 || diag2){
+			 return board[1][1];
+		}
+		
+		return " ";
+	}
+	
+}  // end class
