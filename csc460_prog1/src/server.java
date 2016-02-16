d2cbee9cc6f2b69b36aded73fcf8ba40ce85fc81
Ethan Perry <perrye1@nku.edu>
2016-02-15 21:40:17 -0500

    added comments

--- csc460_prog1/src/server.java
+++ csc460_prog1/src/server.java
@@ -4,33 +4,30 @@ import java.util.Random;
 
 public class server
 {
-
-		
-	@SuppressWarnings("deprecation")
 	public static void main( String[] args)
 	{		
-		ServerSocket SS; // this is the "door" 
+		ServerSocket SS; 
 		
 		Socket toclientsocket;
 
 		
-		try {    // NOTE - must be within a try-clause or throw exceptions!!!!
+		try { 
 	  		
-		SS = new ServerSocket(4030);   //listen at the door
+		SS = new ServerSocket(4030);   //listen for connections on port 4030
 		while (true){
 			System.out.println("waiting for connection");
-			toclientsocket = SS.accept();   // block UNTIL request received
+			toclientsocket = SS.accept();   // block until request received
 			System.out.println("connection made");
 			
-			DataOutputStream out = new DataOutputStream(toclientsocket.getOutputStream( ));
-			DataInputStream in = new DataInputStream(toclientsocket.getInputStream());
+			DataOutputStream out = new DataOutputStream(toclientsocket.getOutputStream( )); //output stream to the client
+			DataInputStream in = new DataInputStream(toclientsocket.getInputStream()); //input stream from the client
 			
-			int moveCount = 0;
+			int moveCount = 0; //keeps track of the number of moves made by both client and server (9 possible)
 			boolean gameOver = false;
 			boolean serverMoveFirst;
 			Random rand = new Random();
 			
-			String board[][] = new String[][]{
+			String board[][] = new String[][]{ //board is a 2d array of strings, starts out filled with spaces
 					{" "," "," "},
 					{" "," "," "},
 					{" "," "," "}
@@ -40,6 +37,7 @@ public class server
 			String serverMove;
 			String parts[];
 			
+			//randomly deciding if the client or the server moves first
 			if(rand.nextInt(2) == 1) {
 			    serverMoveFirst = true;
 			}else{
@@ -47,47 +45,70 @@ public class server
 			}
 			
 			if(serverMoveFirst){
+				
 				//generate a move and send to client
 				serverMove = generateMove(board);
 				out.writeUTF(serverMove);
 				System.out.println("server sent: "+serverMove);
 				moveCount++;
+				
 			}else{
+				
 				//tell the client to move first
 				out.writeUTF("NONE");
 				System.out.println("server sent: NONE");
+				
 			}
 			
 			while(!gameOver==true){
 			
-				clientMove = in.readUTF();
+				clientMove = in.readUTF(); //read in the clients move from the in stream
 				System.out.println("client sent: "+clientMove);
 				parts = clientMove.split(" ");//parse clients move
 				
 				board[Integer.parseInt(parts[1])][Integer.parseInt(parts[2])] = "O";
-				moveCount++;
+				moveCount++; //increment the move count
+				
 				if(checkWin(board).equals("O")){
+					
+					//if the client has won, don't make any more moves and send the win message
 					out.writeUTF("MOVE 0 0 WIN");
 					System.out.println("server sent: MOVE 0 0 WIN");
 					gameOver = true;
+					
 				}else if(moveCount==9){
+					
+					//if there is no winner and the move count is 9, it must be a tie, so send the tie message
 					out.writeUTF("MOVE 0 0 TIE");
 					System.out.println("server sent: MOVE 0 0 TIE");
 					gameOver = true;
+					
 				}else{
+					
+					//if there is no winner and move count is not 9, then we are still playing and it is the servers turn
 					serverMove = generateMove(board);
 					moveCount++;
+					
 					if(checkWin(board).equals("X")){
+						
+						//once the server has moved, if it has won, send the client the loss message
 						out.writeUTF(serverMove+" LOSS");
 						System.out.println("server sent: "+serverMove+" LOSS");
 						gameOver = true;
+						
 					}else if(moveCount==9){
+						
+						//once the server has moved, if it is the 9th move, send the client the tie message
 						out.writeUTF(serverMove+" TIE");
 						System.out.println("server sent: "+serverMove+" TIE");
 						gameOver = true;
+						
 					}else{
+						
+						//once the server has moved, and it is neither a server win or a tie, send the move to the client. Play continues
 						out.writeUTF(serverMove);
 						System.out.println("server sent: "+serverMove);
+						
 					}
 				}
 				
@@ -97,6 +118,7 @@ public class server
     		catch (IOException e) {}  
 	}  // end main
 	
+	//randomly moves to an empty square on the board
 	public static String generateMove(String[][] board){
 		
 		Random rand = new Random();
@@ -113,6 +135,9 @@ public class server
 		
 	}
 	
+	//check if there is a winner by comparing all the cells in each row, column and diagonal
+	//flags h,v,diag1,diag2 are set if any of these checks come back true
+	//if any of the flags are set, we look where it was set, find the piece there, and this must be the winner
 	public static String checkWin(String[][] board){
 		for(int i = 0; i < 3; i++){
 			 boolean h = board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2]);
@@ -131,7 +156,7 @@ public class server
 			 return board[1][1];
 		}
 		
-		return " ";
+		return " "; //if no winner, then return a blank space
 	}
 	
 }  // end class
